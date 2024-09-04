7ae78b547f7aa8532ecf1ceb1e4e20d7e4e636eb
MicheleLafronza <michele.lafronza91@gmail.com>
2024-09-04 22:50:41 +0200

    feat: add base index base

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Pet Shop</title>
+</head>
+<body>
+
+<!-- main container -->
+    <div class="container">
+
+        <!-- titolo principale -->
+        <h1 class="text-center">Pet Shop</h1>
+
+        <!-- sezione cani -->
+        <div class="row">
+
+            <!-- scheda prodotto -->
+            <div class="card col-4">
+                <!-- immagine prodotto -->
+                <img src="..." class="card-img-top" alt="...">
+                <!-- dettagli prodotto -->
+                <div class="card-body">
+                    <h5 class="card-title">Titolo Prodotto</h5>
+                    <p class="card-text">Descrizione prodotto</p>
+                    <i class="fa-solid fa-dog"></i>
+                    <p class="card-text">Prezzo: 1,00$</p>
+                    <a href="#" class="btn btn-primary">Compra</a>
+                </div>
+            </div>
+
+            <!-- scheda prodotto -->
+            <div class="card col-4">
+                <!-- immagine prodotto -->
+                <img src="..." class="card-img-top" alt="...">
+                <!-- dettagli prodotto -->
+                <div class="card-body">
+                    <h5 class="card-title">Titolo Prodotto</h5>
+                    <p class="card-text">Descrizione prodotto</p>
+                    <i class="fa-solid fa-dog"></i>
+                    <p class="card-text">Prezzo: 1,00$</p>
+                    <a href="#" class="btn btn-primary">Compra</a>
+                </div>
+            </div>
+
+            <!-- scheda prodotto -->
+            <div class="card col-4">
+                <!-- immagine prodotto -->
+                <img src="..." class="card-img-top" alt="...">
+                <!-- dettagli prodotto -->
+                <div class="card-body">
+                    <h5 class="card-title">Titolo Prodotto</h5>
+                    <p class="card-text">Descrizione prodotto</p>
+                    <i class="fa-solid fa-dog"></i>
+                    <p class="card-text">Prezzo: 1,00$</p>
+                    <a href="#" class="btn btn-primary">Compra</a>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+</body>
+</html>
